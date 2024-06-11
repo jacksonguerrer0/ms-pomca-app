6ee75e19a8b53a795755fa2284383624c6bbd9c4
jacksonguerrer0 <jacksonguerrerovega2@gmail.com>
2024-06-10 22:35:15 -0500

    structuring app

---
+++ domain/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  verbose: true,
+  reporters: ['default', 'jest-junit'],
+  coverageReporters: ['json', 'lcov', 'text', 'clover']
+}
